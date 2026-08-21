d7dae291636fe894c1a1f01d9fc64d6f4e2b8b23
Aderonmu Adeniyi Bidemi <justone1379@gmail.com>
2026-08-21 09:30:43 +0100

    chore: Move Auth0 config to environment variables

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,15 @@ import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 
+const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN ?? "dev-txt7pa2k5znjr87y.us.auth0.com";
+const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID ?? "stTvahLxOAbCPfNDXfz3jp3sxmCgVNHG";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Auth0Provider
-    domain="dev-txt7pa2k5znjr87y.us.auth0.com"
-    clientId="stTvahLxOAbCPfNDXfz3jp3sxmCgVNHG"
+    domain={AUTH0_DOMAIN}
+    clientId={AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
